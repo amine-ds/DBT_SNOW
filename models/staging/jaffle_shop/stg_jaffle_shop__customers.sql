be55a41edc909f07ecb3d8d8f5b4c9862d89dc3b
Amine BENICHOU <amin.benichou@gmail.com>
2026-02-24 14:16:28 +0000

    khc

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,15 +1,12 @@
 with 
-
 source as (
-
     select * from {{ source('jaffle_shop', 'customers') }}
-
 ),
 
 renamed as (
 
     select
-        id,
+        id as customer_id,
         first_name,
         last_name
 
